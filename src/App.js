e6e6ed1d9f3368617a868efb1cfa9325d6ac83a5
jennieca07 <168984490+jennieca07@users.noreply.github.com>
2024-10-08 11:08:59 +0545

    Add form section for reporting and checking frauds

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Heading from './components/Heading';
-<<<<<<< Updated upstream
-import InsightsSection from './components/InsightsSection'; // Import the new section
-import './App.css';
-=======
+import SRsection from './components/SRsection';
 import InsightsSection from './components/InsightsSection'; // Import the Insights Section
->>>>>>> Stashed changes
 import BlogSection from './components/BlogSection'; // Adjust the path if necessary
 import FAQSection from './components/FAQSection';
 import FooterSection from './components/FooterSection';
-import SRsection from './components/SRsection';
 
 function App() {
   return (
     <div className="App" style={{ backgroundColor: '#181818' }}> {/* Set background color inline */}
       <Navbar />
       <Heading />
-<<<<<<< Updated upstream
-=======
       <SRsection />
->>>>>>> Stashed changes
       <InsightsSection />
       <BlogSection />
       <FAQSection />
