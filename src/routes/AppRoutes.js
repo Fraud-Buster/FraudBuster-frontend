42462dcae3b164fabc91d7f243025e0c94d99ee6
jennieca07 <168984490+jennieca07@users.noreply.github.com>
2024-10-15 02:56:00 +0545

    Fix Error

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import LandingPage from './LandingPage'; // Adjust the import path as needed
-// import About from './About'; // Adjust the import path as needed
-// import Blogs from './Blogs'; // Adjust the import path as needed
-// import Contact from './Contact'; // Adjust the import path as needed
-// import API from './API'; // Adjust the import path as needed
 import { Home } from '../pages/Home';
 import AboutPage from '../pages/About';
 import BlogPage from '../pages/Blog';
 import ContactPage from '../pages/ContactUs';
 import { NotFoundPage } from '../pages/NotFoundPage';
+
 export const AppRoutes = () => {
   return (
+    <Router>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<AboutPage/>} />
-        <Route path="/blogs" element={<BlogPage/>} />
-        <Route path="/contact" element={<ContactPage/>} />
-        <Route path="/api" element={<NotFoundPage/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/blogs" element={<BlogPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="*" element={<NotFoundPage />} /> {/* Catch-all route for 404 */}
       </Routes>
+    </Router>
   );
 };
-
-
